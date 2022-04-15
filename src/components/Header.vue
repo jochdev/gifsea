393f8838fd1a4df4546d4116aec9499e3d96723b
Jose Heredia <hola@joch.dev>
2022-04-15 19:16:05 -0400

    Nuevo header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,22 @@
 <template>
-<div class="container">
-    <header class="d-flex flex-wrap justify-content-center py-3 mb-4">
-      <router-link to="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none"><span class="fs-4 txt-color">Gif<span class="color-secundadrio">sea</span></span></router-link>
-      <ul class="nav nav-pills">
-        <router-link to="/gifs" class="orange nav-link px-2 txt-color">Gifs</router-link>
-        <router-link to="/stickers" class="orange nav-link px-2 txt-color">Stickers</router-link>
-        <router-link to="/faq" class="orange nav-link px-2 txt-color">FAQ</router-link>
+  <div class="container">
+    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3">
+      <router-link to="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+        <span class="fs-4 txt-color">Gif<span class="color-secundadrio">sea</span></span>
+      </router-link>
+      <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+        <li class="nav-item">
+          <router-link to="/" class="nav-link px-2 txt-color orange">Inicio</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/gifs" class="nav-link px-2 txt-color orange">Gifs</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/stickers" class="nav-link px-2 txt-color orange">Stickers</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/faq" class="nav-link px-2 txt-color orange">FAQ</router-link>
+        </li>
       </ul>
     </header>
   </div>
@@ -19,13 +30,16 @@
   .menu {
     background-color: #eeeeee;
   }
+
   .txt-color {
     color: #06113c;
   }
+
   .router-link-exact-active {
     display: inline-block;
     position: relative;
   }
+
   .router-link-exact-active::after {
     display: block;
     position: absolute;
@@ -36,6 +50,7 @@
     content: "";
     opacity: 0.9;
   }
+
   .router-link-exact-active.orange::after {
     background-color: #ff8c32;
   }
